4601f705d7d99adaa6095d4cb75ed2fe5813a694
vladiim <vladiim@yahoo.com.au>
2013-02-08 08:15:51 +1100

    Interest::Bearing #calc_debt_at_maturity calculates debt_at_maturity

--- lib/interest/lib/bearing.rb
+++ lib/interest/lib/bearing.rb
@@ -3,6 +3,12 @@ require 'date'
 
 # named as in interest bearing loan
 
+# the present value of a debit involves one rate and bears interest
+# at another rate
+
+# the present value of the maturity uses the rate of discount
+# also known as discount_rate or by expression 'money is worth'
+
 # process to solving interest-beaing debt problems:
 # 1. determine the date of maturity of the note
 # 2. compute the value of the debt @ maturity
@@ -27,19 +33,20 @@ class Interest::Bearing
   attr_accessor :duration,       :commencement_date, :total_sum,
                 :annual_rate,    :date_of_maturity,  :discount_start,
                 :monetery_value, :principal,         :discount_end,
-                :discount_period
+                :discount_rate,  :discount_period,   :debt_at_maturity
 
   def calc_debt_at_maturity
   	@date_of_maturity = Date.parse(@commencement_date) + 90
-  	@total_sum = calc_total_sum
-  	@discount_period = calc_discount_period
+  	@total_sum        = calc_total_sum
+  	@discount_period  = calc_discount_period
+  	@debt_at_maturity = (@total_sum / ( 1 + (discount_rate_as_percentage * calc_discounted_term) )).round(2)
   end
 
   private
 
   def calc_total_sum
   	(raise NeedTotalSumError, "Need @annual_rate, @duration and @principal") unless total_sum_variables
-  	(@principal * (1 + (annual_rate_as_percentage * calc_term)))
+  	(@principal * (1 + (annual_rate_as_percentage * calc_initial_term)))
   end
 
   def calc_discount_period
@@ -55,13 +62,19 @@ class Interest::Bearing
   	@discount_start && @discount_end
   end
 
-  def calc_term
+  def calc_initial_term
   	(@duration.round(2) / 365.round(2)).round(2)
   end
 
+  def calc_discounted_term
+    (@discount_period.round(2) / 365.round(2)).round(7)
+  end
 
   def annual_rate_as_percentage
   	@annual_rate.round(2) / 100
   end
-  # d = Date.parse('3rd Feb 2001')
+
+  def discount_rate_as_percentage
+  	@discount_rate.round(2) / 100
+  end
 end
--- test/interest/bearing_test.rb
+++ test/interest/bearing_test.rb
@@ -13,6 +13,7 @@ describe Interest::Bearing do
   	  @interest.duration          = 90 # days
   	  @interest.commencement_date = '18th May 2013'
   	  @interest.annual_rate       = 7   # %
+      @interest.discount_rate     = 7.5 # %
       @interest.discount_start    = '6th July 2013'
       @interest.discount_end      = '16th August 2013'
       @interest.calc_debt_at_maturity
@@ -26,8 +27,8 @@ describe Interest::Bearing do
       @interest.discount_period.must_equal 41
     end
 
-    # it 'calculates the debt_at_maturity as 807.20' do
-    #   @interest.debt_at_maturity.must_equal 807.20
-    # end
+    it 'calculates the debt_at_maturity as 807.20' do
+      @interest.debt_at_maturity.must_equal 807.20
+    end
   end
 end
