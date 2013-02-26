93fa36d946a12b5ffa05c98836a8ab2a5ce794a5
vladiim <vladiim@yahoo.com.au>
2013-02-27 08:25:22 +1100

    Interest::Compound#calc_accumulated_sum works

--- lib/interest/lib/compound.rb
+++ lib/interest/lib/compound.rb
@@ -1,17 +1,62 @@
 require_relative '../interest'
 
-class Interest::Compound
+# annual compound interest = interest is paid/added to the 
+# principal once per year
+# otherwise converted at other regular periods, number of times
+# it's converted p/a = frequency_of_conversion
 
-  attr_accessor :loan, :interest,   :length_months,
-                :repayment_ammount, :repayment_interest
+class Interest::Compound
+  class VariableMissingError < StandardError; end
+
+  attr_accessor :principal
+
+  attr_accessor :interest,      :repayment_interest,
+                :length_months, :repayment_ammount
   def calc_repayment_ammount
-  	# repayment_ammount_error unless repayment_ammount_variables
+  	repayment_ammount_error unless repayment_ammount_variables
     @repayment_interest = calc_repayment_interest
+    @repayment_ammount  = repayment_interest + principal
+  end
+
+  attr_accessor :periods, :interest_per_period, :compound_ammount
+  def calc_accumulated_sum
+    accumulated_sum_error unless accumulated_sum_variables
+    @compound_ammount = (principal *
+      (1 + as_percentage(interest_per_period)) **
+      periods).round(2)
   end
 
   private
 
   def calc_repayment_interest
-    loan * (interest.round(2)/100) * (length_months.round(2)/12)
+    principal * as_percentage(interest) * in_years(length_months)
+  end
+
+  def as_percentage(variable)
+    variable.round(2)/100
+  end
+
+  def in_years(months)
+    months.round(2)/12
+  end
+
+  def repayment_ammount_variables
+    principal && interest && length_months
+  end
+
+  def accumulated_sum_variables
+    principal && periods && interest_per_period
+  end
+
+  def repayment_ammount_error
+    variable_missing_error_raiser(repayment_ammount_variables)
+  end
+
+  def accumulated_sum_error
+    variable_missing_error_raiser(accumulated_sum_variables)
+  end
+
+  def variable_missing_error_raiser(variables)
+    raise VariableMissingError, "Need all: #{variables}"
   end
 end
--- test/interest/compound_test.rb
+++ test/interest/compound_test.rb
@@ -2,22 +2,72 @@ require_relative '../test_helper'
 require_relative '../../lib/interest/lib/compound'
 
 describe Interest::Compound do
-  before { @compound = Interest::Compound.new }
+  let(:compound) { Interest::Compound.new }
+  let(:error)    { Interest::Compound::VariableMissingError }
 
   describe '#calc_repayment_ammount' do
   	before do
-  	  @compound.loan          = 1000
-  	  @compound.interest      = 6
-  	  @compound.length_months = 6
-  	  @compound.calc_repayment_ammount
+  	  compound.principal     = 1000
+  	  compound.interest      = 6
+  	  compound.length_months = 6
   	end
 
-    it 'repayment_interest is 30' do
-      @compound.repayment_interest.must_equal 30
+    describe 'with all variables' do
+      before { compound.calc_repayment_ammount }
+
+      it 'repayment_interest is 30' do
+        compound.repayment_interest.must_equal 30
+      end
+
+      it 'repayment_ammount is 1030' do
+        compound.repayment_ammount.must_equal 1030
+      end
     end
 
-    it 'repayment_ammount is 1030' do
-      @compound.repayment_ammount.must_equal 1030
+    describe 'variable missing' do
+      let(:vars) { %w( principal interest length_months ) }
+
+      it 'raises an error' do
+        vars.each do |var|
+          compound.send("#{var}=", nil)
+          ->{ compound.calc_repayment_ammount }.must_raise error
+        end
+      end
+    end
+  end
+
+  describe '#calc_accumulated_sum' do
+    before do
+      compound.principal = 2000
+      compound.periods   = 5
+      compound.interest_per_period = 2.5
+    end
+
+    describe 'with all variables' do
+      before { compound.calc_accumulated_sum }
+
+      it 'compound_ammount is 2262.82' do
+        compound.compound_ammount.must_equal 2262.82
+      end
+    end
+
+    describe 'variable missing' do
+      let(:vars) { %w( principal periods interest_per_period ) }
+
+      it 'raises an error' do
+        vars.each do |var|
+          compound.send("#{var}=", nil)
+          ->{ compound.calc_accumulated_sum }.must_raise error
+        end
+      end
     end
   end
 end
+
+# P = principal
+# i = interest per period
+# I = total interest
+# n = number of interest periods
+# S = compound ammount, sum of compounded principal & interest
+# m = freq of conversion
+# j = nominal (annual) interest rate
